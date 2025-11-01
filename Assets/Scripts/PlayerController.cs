d7fff36751038e8f35b5f01d36a898d1b83edf31
gcesario203 <gcesario203@gmail.com>
2025-11-01 16:16:10 -0300

    feat: animações e maquinas finitas de estado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,45 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rigidBody;
     public float speed = 5f;
     public float jumpForce = 10f;
+
+    private bool isGrounded = false;
+
+    private Animator animator;
+
+    private SpriteRenderer spriteRenderer;
     
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         if (Input.GetAxis("Horizontal") != 0)
         {
-            rigidBody.linearVelocity = 
-                new Vector2(Input.GetAxis("Horizontal") * speed, 
-                    rigidBody.linearVelocity.y);
-        }
+            rigidBody.linearVelocityX = Input.GetAxis("Horizontal") * speed;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetBool("waliking", true);
+
+            spriteRenderer.flipX = Input.GetAxis("Horizontal") * speed < 0;
+        }
+        else
+            animator.SetBool("waliking", false);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            isGrounded = false;
             rigidBody.AddForceY(jumpForce);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D anotherObject)
+    {
+        if (anotherObject.gameObject.layer.Equals(LayerMask.NameToLayer("Ground")))
+            isGrounded = true;
     }
 }
